d201464a56d40a259145499f22508fbca757b8f6
simo <svm.zhang@gmail.com>
2024-04-26 11:17:37 -0700

    [Major Update] Filled in codes to run each step

--- scripts/hlafinalizer.sh
+++ scripts/hlafinalizer.sh
@@ -32,10 +32,10 @@ EO
 }
 
 sample=
-bam=
+realn_dir=
 typing_res=
 hla_ref=
-out_bam=
+outdir=
 nproc=8
 
 if [ "$#" -le 1 ]; then
@@ -53,9 +53,9 @@ while [ $# -gt 0 ]; do
 		shift
 		sample="$1"
 		;;
-  --bam)
+  --realn_dir)
     shift
-    bam=$(parse_path "$1")
+    realn_dir="$1"
     ;;
   --typeres)
     shift
@@ -65,9 +65,9 @@ while [ $# -gt 0 ]; do
 		shift
 		hla_ref=$(parse_path "$1")
 		;;
-	-o | --out)
+	--outdir)
 		shift
-		out_bam="$1"
+		outdir="$1"
 		;;
 	-j | --nproc)
 		shift
@@ -86,15 +86,75 @@ while [ $# -gt 0 ]; do
 	shift
 done
 
+# FIXME: make a final folder
+check_dir_exists "$realn_dir"
+outdir=$( make_dir "$outdir" )
+logdir="$outdir/log"
+logdir=$( make_dir "$logdir" )
+
 # 1.1 get sample-level HLA reference based on hlatyping result
+info "$0" "$LINENO" "Get sample-level HLA reference sequence"
+hlatypelist_file="$outdir/.$sample.hlatypelist.txt"
+tail -n +2 "$typing_res" | cut -d $'\t' -f 2- | sed 's/\t/\n/g' > "$hlatypelist_file" 
+nexpect=$( wc -l <"$hlatypelist_file" )
+
+sample_hla_ref="$outdir/$sample.hla.fasta"
+cmd=("seqkit" "grep" "-f" "$hlatypelist_file" "-o" "$sample_hla_ref" "$hla_ref" )
+if ! "${cmd[@]}" >/dev/null 2>&1;
+then
+	error "$0" "$LINENO" "Failed to get sample-level HLA reference sequence in a Fasta file"
+	exit 1
+fi
+
+# check if Fasta is empty
+if [ ! -s "$sample_hla_ref" ];
+then
+	error "$0" "$LINENO" "Failed to extract any sequences in ${sample_hla_ref##*/}"
+	exit 1
+fi
+
+# check if sequences from 6 alleles were generated
+nseq=$( grep -c ">" "$sample_hla_ref" )
+if [ "$nseq" -ne "$nexpect" ];
+then
+	error "$0" "$LINENO" "Expect to extract $nexpect alleles, but got $nseq"
+	exit 1
+fi
+info "$0" "$LINENO" "Get sample-level HLA reference sequence [DONE]"
 
 # 1.2 make novoindex off the reference
+info "$0" "$LINENO" "Index HLA reference using novoindex"
+sample_hla_ref_nix="${sample_hla_ref%.fasta}.nix"
+cmd=("novoindex" "$sample_hla_ref_nix" "$sample_hla_ref")
+if ! "${cmd[@]}" >/dev/null 2>&1;
+then
+	error "$0" "$LINENO" "Failed to index the sample-level HLA reference: ${sample_hla_ref##*/}"
+	exit 1
+fi
+info "$0" "$LINENO" "Index HLA reference using novoindex [DONE]"
 
-# 1.3 bam2fq (if I decide to do this way)
-# or we could have the file ready from razer_realigner.sh
+# 1.3 run_novoalign_batch function
+info "$0" "$LINENO" "Realign paired reads to sample-level HLA reference using Novoalign"
+pair_dir="$realn_dir/pair"
+novo_dir="$outdir/novoalign"
+novo_dir=$( make_dir "$novo_dir" )
+check_dir_exists "$pair_dir"
+fq_search_regex=".part_*.fastq"
+run_novoalign_batch "$pair_dir" "$novo_dir" "$fq_search_regex" "$sample_hla_ref_nix" "$nproc"
+info "$0" "$LINENO" "Realign paired reads to sample-level HLA reference using Novoalign [DONE]"
 
-# 1.4 split fastq files using run_seqkit_split2 function
+# 1.4 concat bam file
+info "$0" ${LINENO} "Concatenate individually novoaligned BAM files" 
+bam_search_regex="*.bam"
+realn_bam="$outdir/$sample.hla.realn.bam"
+run_samtools_cat "$novo_dir" "$bam_search_regex" "$realn_bam"
+info "$0" ${LINENO} "Concatenate individually novoaligned BAM files [DONE]" 
 
-# 1.5 run_novoalign_batch function
+# 1.4 concat bam file
+info "$0" ${LINENO} "Post-process realigned BAM file" 
+realn_so_bam="${realn_bam%.bam}.so.bam"
+run_samtools_sort "$realn_bam" "$realn_so_bam" "$nproc"
+info "$0" ${LINENO} "Post-process realigned BAM file [DONE]" 
+info "$0" ${LINENO} "Run Polysolver HLA realigner [DONE]" 
 
-# 1.6 concat bam file
+exit 0
