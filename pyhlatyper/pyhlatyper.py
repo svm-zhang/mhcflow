99ea624b17a5e459632c7e26bda2bbb27f4c31ee
simo <svm.zhang@gmail.com>
2024-05-23 20:52:06 -0400

    Implement score function for both alleles; now it calls the same hla type as origianl perl script

--- pyhlatyper/pyhlatyper.py
+++ pyhlatyper/pyhlatyper.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
 import argparse
-from dataclasses import dataclass
 import itertools
 import math
 import numpy as np
+import os
 import polars as pl
 import pysam
+import re
 import sys
 
 
@@ -33,6 +34,13 @@ def parse_cmd() -> argparse.ArgumentParser:
         type=str,
         default="Unknown",
         help="specify path to HLA frequency file",
+    )
+    parser.add_argument(
+        "--outdir",
+        metavar="DIR",
+        type=str,
+        required=True,
+        help="specify path to output directory",
     )
 
     return parser
@@ -86,11 +94,28 @@ def score_log_liklihood(base_qs, md, scale=math.exp(23)):
     return score
 
 
-def extract_alignments(bam: str, allele: str, freq_df: pl.DataFrame):
+def extract_gene_from_allele(allele: str) -> str:
+    allele = allele.lower()
+    # A*01:01:01, hla_a_01_01_01
+    return re.sub("(\\*|_)*([0-9]+[a-z]*(:|_)*)", "", allele)
+
+
+def extract_supertype_from_allele(allele: str) -> str:
+    allele = allele.lower()
+    a_iter = itertools.groupby(allele, lambda k: k.isalnum())
+    return "_".join(
+        ["".join(group) for i, (c, group) in enumerate(a_iter) if c and i < 8]
+    )
+
+
+def extract_alignments(
+    bam: str, allele: str, freq_df: pl.DataFrame
+) -> pl.DataFrame | None:
+    hla_gene = extract_gene_from_allele(allele)
     bamf = pysam.AlignmentFile(bam, "rb")
     n_reads = bamf.count(contig=allele)
     if n_reads < 50:
-        return
+        return None
 
     scores: list[float] = []
     ids: list[str] = []
@@ -120,23 +145,41 @@ def extract_alignments(bam: str, allele: str, freq_df: pl.DataFrame):
             "scores": scores,
         },
     )
-    res_df = res_df.groupby("ids").agg(pl.col("scores").sum())
-    res_df = res_df.with_columns(allele=pl.lit(allele))
+    # this makes sure only paired reads counted towards final score
+    res_df = res_df.with_columns(pl.col("ids").count().over("ids").alias("n"))
+    res_df = res_df.filter(pl.col("n") == 2)
+    res_df = res_df.drop("n")
     # FIXME: need to add frequency prior to the log-liklihood score
-    tot_df = pl.DataFrame(
-        {
-            "ids": ["total"],
-            "scores": [res_df["scores"].sum()],
-            "allele": [allele],
-        }
-    )
-    res_df = pl.concat([res_df, tot_df])
-    res_df = res_df.with_columns(pl.col("scores").cast(pl.Float32))
+    res_df = res_df.groupby("ids").agg(pl.col("scores").sum())
+    res_df = res_df.with_columns(allele=pl.lit(allele), gene=pl.lit(hla_gene))
     print(allele)
-    # print(res_df.sort(by="ids").head(10))
-    # print(res_df.filter(pl.col("ids") == "total"))
-    # if allele == "hla_a_01_01_02":
-    #    sys.exit()
+    return res_df
+
+
+def get_winners(allele_scores: pl.DataFrame) -> pl.DataFrame:
+    tot_scores = allele_scores.groupby(["allele", "gene"]).agg(
+        pl.col("scores").sum()
+    )
+    winners = tot_scores.filter(
+        pl.col("scores") == pl.col("scores").max().over("gene")
+    )
+    winners = winners.rename({"scores": "tot_scores"})
+    return winners
+
+
+def score_second(score_table: pl.DataFrame, a1_winners: pl.DataFrame):
+
+    score_table = score_table.join(a1_winners, on=["ids", "gene"], how="left")
+    score_table = score_table.with_columns(
+        pl.col("scores_right").fill_null(0.0)
+    )
+    score_table = score_table.with_columns(
+        factor=pl.col("scores") / (pl.col("scores") + pl.col("scores_right"))
+    )
+    score_table = score_table.with_columns(
+        scores=pl.col("scores") * pl.col("factor")
+    )
+    return score_table
 
 
 def main():
@@ -145,13 +188,58 @@ def main():
     args = parser.parse_args()
 
     freq_df = pl.read_csv(args.freq, separator="\t")
-    print(freq_df.head())
 
     alleles = get_alleles(bam=args.bam)
-    print(alleles[0:5])
+    alleles_df = pl.DataFrame({"Allele": alleles})
+    alleles_df = alleles_df.with_columns(
+        pl.col("Allele")
+        .apply(extract_supertype_from_allele)
+        .alias("SuperType")
+    )
+    alleles_df = alleles_df.join(
+        freq_df, left_on="SuperType", right_on="Allele", how="inner"
+    )
+    alleles = alleles_df["Allele"].to_list()
 
-    for allele in alleles:
-        extract_alignments(bam=args.bam, allele=allele, freq_df=freq_df)
+    out_a1 = f"{args.outdir}/test.a1.tsv"
+    out_a2 = f"{args.outdir}/test.a2.tsv"
+
+    a1_scores = pl.DataFrame()
+    if not os.path.exists(out_a1):
+        print("score first allele")
+        score_tables: list[pl.DataFrame] = []
+        for allele in alleles:
+            score_tables.append(
+                extract_alignments(
+                    bam=args.bam, allele=allele, freq_df=freq_df
+                )
+            )
+        a1_scores = pl.concat([s for s in score_tables if s is not None])
+        a1_scores.write_csv(out_a1, separator="\t")
+
+    a1_scores = pl.read_csv(out_a1, separator="\t")
+
+    a1_winners = get_winners(allele_scores=a1_scores)
+    winner_scores = a1_scores.join(
+        a1_winners, on=["gene", "allele"], how="inner"
+    )
+    if not os.path.exists(out_a2):
+        print("score second allele")
+        a2_score_tables: list[pl.DataFrame] = []
+        for gene in a1_winners["gene"].unique().to_list():
+            a2_score_tables.append(
+                score_second(
+                    a1_scores.filter(pl.col("gene") == gene),
+                    winner_scores.filter(pl.col("gene") == gene),
+                )
+            )
+        a2_scores = pl.concat(a2_score_tables)
+        a2_scores.write_csv(out_a2, separator="\t")
+
+    a2_scores = pl.read_csv(out_a2, separator="\t")
+    a2_winners = get_winners(allele_scores=a2_scores)
+    print(a1_winners)
+    print(a2_winners)
 
 
 if __name__ == "__main__":
