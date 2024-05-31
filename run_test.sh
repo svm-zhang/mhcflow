7f03f2cf01eaf79a1eed93d77c64939ec0952e54
simo <svm.zhang@gmail.com>
2024-05-30 20:59:05 -0400

    Add import test for tqdm

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 
-python3 -c "import numpy as np; import polars as pl; import pysam"
+python3 -c "import numpy as np; import polars as pl"
+
+python3 -c "import pysam"
+
+python3 -c "from tqdm import tqdm"
