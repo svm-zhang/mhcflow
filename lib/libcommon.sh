8b7a1ec5cb9ec4514a558f08375974a445456634
simo <svm.zhang@gmail.com>
2024-06-12 18:37:15 -0400

    1. add func to check empty variable; 2. update parse_path to get_abs_path and able to handle both dir and file cases; 3. remove run_cmd func

--- lib/libcommon.sh
+++ lib/libcommon.sh
@@ -3,11 +3,11 @@
 set -e
 
 function error () {
-	echo "[${1##*/}:$2] ERROR: $3" 1>&2
+	echo "[${1##*/}:$2] ERROR - $3" 1>&2
 }
 
 function info () {
-	echo "[${1##*/}:$2] INFO: $3"
+	echo "[${1##*/}:$2] INFO - $3"
 }
 
 function die () {
@@ -18,8 +18,23 @@ function die () {
 	exit "$(( rc == 0 ? 1 : rc ))"
 }
 
-function parse_path () {
-	check_file_exists "$1"
+function check_empty_str () {
+	local var="$1"
+	if [ -z "$var" ]; then
+		return 1
+	fi
+}
+
+function get_abs_path () {
+	case "$2" in
+		d) check_dir_exists "$path" \
+			|| die "$0" "$LINENO" \
+				"Failed to generate abs path b/c non-existence of provided dir path $1" ;;
+		f) check_file_exists "$1" \
+			|| die "$0" "$LINENO" \
+				"Failed to generate abs path b/c non-existence of provided file path $1" ;;
+		*) die "$0" "$LINENO" "Unrecognized type passing in get_abs_path: $2";;
+	esac
 	local abs_path
 	abs_path=$(cd -P -- "$(dirname -- "$1")" && printf '%s\n' "$(pwd -P)/$(basename -- "$1")")
 	echo "$abs_path"
@@ -29,7 +44,7 @@ function check_file_exists () {
 	local paths=("$@")
 	for f in "${paths[@]}"; do
 		if [ ! -f "$f" ]; then
-			error "${FUNCNAME[0]}" ${LINENO} "Failed to find the file path at the given path: ${f}"
+			#error "${FUNCNAME[0]}" ${LINENO} "Failed to find the file path at the given path: ${f}"
 			return 1
 		fi
 	done
@@ -40,37 +55,27 @@ function check_dir_exists () {
 	local paths=("$@")
 	for d in "${paths[@]}"; do
 		if [ ! -d "$d" ]; then
-			error "$0" ${LINENO} "Failed to find directory for the given path: ${d}"
-			exit 1
+			#error "$0" ${LINENO} "Failed to find directory for the given path: ${d}"
+			#exit 1
+			return 1
 		fi
 	done
+	return 0
 }
 
 function make_dir () {
-
-	if [ ! -d "$1" ];
+	local path="$1"
+	if [ ! -d "$path" ];
 	then
-		mkdir -p "$1"	
+		mkdir -p "$path"	
 	fi
+	abs_path=$( get_abs_path "$path" "d")
+	echo "$abs_path"
 
-	local dir_path
-	dir_path=$(cd -P -- "$(dirname -- "$1")" && printf '%s\n' "$(pwd -P)/$(basename -- "$1")")
-	echo "$dir_path"
+	#local dir_path
+	#dir_path=$(cd -P -- "$(dirname -- "$1")" && printf '%s\n' "$(pwd -P)/$(basename -- "$1")")
+	#echo "$dir_path"
 }
 
-function run_cmd () {
-
-	cmd="$1"
-	where="$2"
-	err_msg="$3"
-
-	if ! eval "$cmd";
-	then
-		error "$0" "$where" "$err_msg"
-		error "$0" "$where" "Please check command: \"$cmd\""
-		exit 255 
-	fi
-
-}
-
-export -f info error die parse_path check_file_exists check_dir_exists make_dir run_cmd
+export -f info error die
+export -f get_abs_path check_file_exists check_dir_exists make_dir
