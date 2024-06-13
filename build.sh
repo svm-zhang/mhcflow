e9ba44fc51f41ed21b7aef86e9e9669b70b1d1eb
simo <svm.zhang@gmail.com>
2024-06-13 12:53:14 -0400

    Update binary name

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
 
 # copy scripts to bin folder
-cp "$RECIPE_DIR/scripts/hlapolysolver.sh" "$PREFIX/bin/hlapolysolver"
+cp "$RECIPE_DIR/scripts/hlapolysolver.sh" "$PREFIX/bin/polysolvermod"
 cp "$RECIPE_DIR/scripts/fisher.sh" "$PREFIX/bin/fisher"
-cp "$RECIPE_DIR/scripts/polysolver_realigner.sh" "$PREFIX/bin/polysolver_realigner"
-cp "$RECIPE_DIR/scripts/extract_sample_hlaref.sh" "$PREFIX/bin/extract_sample_hlaref"
+cp "$RECIPE_DIR/scripts/polysolver_realigner.sh" "$PREFIX/bin/realigner"
+cp "$RECIPE_DIR/scripts/extract_sample_hlaref.sh" "$PREFIX/bin/extractor"
 cp "$RECIPE_DIR/pyhlatyper/pyhlatyper.py" "$PREFIX/bin/pyhlatyper"
 
 # make it executable
