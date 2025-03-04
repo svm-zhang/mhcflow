7ebcdf282ff7c0df4d86cf320ebb803f598470c1
simo <svm.zhang@gmail.com>
2025-03-04 09:45:21 -0800

    Updated test as using mhctyper now.

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# python3 -c "import numpy as np; import polars as pl"
+#
+# python3 -c "import pysam"
+#
+# python3 -c "from tqdm import tqdm"
+#
 
-python3 -c "import numpy as np; import polars as pl"
-
-python3 -c "import pysam"
-
-python3 -c "from tqdm import tqdm"
+python3 -c "import mhctyper; print(mhctyper.__version__)"
