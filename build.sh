1966e020ad2de74331be62d94aeecbca16130dff
simo <svm.zhang@gmail.com>
2025-03-04 09:45:01 -0800

    Updated name.

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
 
 # copy scripts to bin folder
-cp "$RECIPE_DIR/scripts/hlapolysolver.sh" "$PREFIX/bin/polysolvermod"
+cp "$RECIPE_DIR/scripts/mhcflow.sh" "$PREFIX/bin/mhcflow"
 cp "$RECIPE_DIR/scripts/fisher.sh" "$PREFIX/bin/fisher"
-cp "$RECIPE_DIR/scripts/polysolver_realigner.sh" "$PREFIX/bin/realigner"
+cp "$RECIPE_DIR/scripts/mhcrealigner.sh" "$PREFIX/bin/realigner"
 cp "$RECIPE_DIR/scripts/extract_sample_hlaref.sh" "$PREFIX/bin/extractor"
-cp "$RECIPE_DIR/pyhlatyper/pyhlatyper.py" "$PREFIX/bin/pyhlatyper"
+# cp "$RECIPE_DIR/pyhlatyper/pyhlatyper.py" "$PREFIX/bin/pyhlatyper"
 
 # make it executable
 chmod +x "$PREFIX"/bin/*
@@ -19,3 +19,7 @@ chmod +x "$PREFIX"/bin/*
 cp "$RECIPE_DIR/lib/libcommon.sh" "$PREFIX/lib"
 cp "$RECIPE_DIR/lib/librealign.sh" "$PREFIX/lib"
 cp "$RECIPE_DIR/lib/libbamer.sh" "$PREFIX/lib"
+
+set -x
+"$PREFIX/bin/pip3" install "mhctyper>=0.1.4" -vvv
+set +x
