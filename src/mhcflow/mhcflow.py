87c7bef084f993d944f30bbb0d5b7f8c943be965
simo <svm.zhang@gmail.com>
2025-03-06 23:19:20 -0800

    initial commit.

---
+++ src/mhcflow/mhcflow.py
@@ -0,0 +1,16 @@
+from mhctyper import run_mhctyper
+
+
+def run_mhcflow():
+    # run strawlr to get read ids
+
+    # get read sequence
+
+    # realign them using novoalign
+
+    # call mhctyper
+    # FIXME: update mhctyper to also make it library
+    run_mhctyper()
+
+    # get result and finalize
+    pass
