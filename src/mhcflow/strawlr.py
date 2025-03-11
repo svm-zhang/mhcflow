bd78bc7b8f7695e6abb2cb796525061d491a2630
simo <svm.zhang@gmail.com>
2025-03-11 08:42:26 -0700

    Implemented all _fish_* and run_strawlr functions.

--- src/mhcflow/strawlr.py
+++ src/mhcflow/strawlr.py
@@ -1,31 +1,19 @@
+import multiprocessing as mp
 import time
 from functools import partial
-from multiprocessing import get_context
 
-import polars as pl
+import numpy as np
 import pysam
-from tinyscibio import _PathLike, parse_path, walk_bam
+from tinyscibio import BAMetadata, _PathLike, bed_to_df, walk_bam
 
 from .logger import logger
 from .tag_builder import build
 
-
-def _get_qnames_from_seq_with_tag(bam_fspath, prebuilt_tag) -> set[str]:
-    start_t = time.time()
-    qnames = set()
-    with pysam.AlignmentFile(str(bam_fspath), "rb") as bamf:
-        for aln in bamf.fetch("6"):
-            assert aln.query_sequence is not None
-            match = list(prebuilt_tag.iter(aln.query_sequence))
-            if match:
-                qnames.add(aln.query_name)
-    print(f"{time.time() - start_t} sec")
-    return qnames
+CHR6 = ["6", "chr6", "NC00006", "CM000668"]
 
 
-def _get_qnames_from_unplace_seq_with_tag(
-    bam_fspath, prebuilt_tag
-) -> set[str]:
+def _fish_unplaced(bam_fspath, prebuilt_tag) -> set[str]:
+    logger.info("Fish unplaced sequence with tag pattern")
     qnames = set()
     start_t = time.time()
     with pysam.AlignmentFile(bam_fspath, "rb") as bamf:
@@ -36,45 +24,123 @@ def _get_qnames_from_unplace_seq_with_tag(
             match = list(prebuilt_tag.iter(aln.query_sequence))
             if match:
                 qnames.add(aln.query_name)
-    print(f"{time.time() - start_t} sec")
-    print(len(qnames))
+    logger.info(
+        f"Fish unplaced sequence with tag pattern: {time.time() - start_t} sec"
+    )
+    logger.info(f"Fished {len(qnames)} unplaced sequence with tag pattern")
     return qnames
 
 
-def _get_qnames_from_seq_mapped_to_class1(bam_fspath) -> set[str]:
-    regions = [
-        ("6", 29909037, 29913661),
-        ("6", 31236526, 31239869),
-        ("6", 31321649, 31324964),
-    ]
+def _fish_one_hla(region: str, bam_fspath: str):
+    return walk_bam(bam_fspath, region, exclude=0, return_qname=True)
+
+
+def _fish_multi_hla(
+    bed_fsapth: _PathLike, bam_fspath: str, nproc: int = 4
+) -> set[str]:
+    logger.info(f"Fish sequence mapped to regions defined in {bed_fsapth}.")
+    df = bed_to_df(bed_fsapth)
+    regions = [f"{row[0]}:{row[1]}-{row[2]}" for row in df.rows()]
+    nproc = min(len(regions), nproc)  # nproc set to minimum of these 2 values
     qnames = set()
     start_t = time.time()
-    with pysam.AlignmentFile(bam_fspath, "rb") as bamf:
-        for region in regions:
-            contig, start, end = region
-            for aln in bamf.fetch(contig, start, end):
-                qnames.add(aln.query_name)
-    print(f"{time.time() - start_t} sec")
-    print(len(qnames))
+    with mp.get_context("spawn").Pool(processes=nproc) as pool:
+        for res in pool.imap_unordered(
+            partial(_fish_one_hla, bam_fspath=bam_fspath),
+            regions,
+        ):
+            if res is not None:
+                qnames = qnames | set(res["qnames"].to_list())
+    logger.info(
+        "Fished sequence mapped to regions defined in BED file: "
+        f"{time.time() - start_t} sec."
+    )
+    logger.info(
+        f"Fished {len(qnames)} mapped to HLA regions defined in BED file"
+    )
     return qnames
+
+
+def _fish_one_region(region, bam_fspath, prebuilt_tag) -> set[str]:
+    qnames = set()
+    sn, start, end = region
+    with pysam.AlignmentFile(str(bam_fspath), "rb") as bamf:
+        for aln in bamf.fetch(contig=sn, start=start, stop=end):
+            assert aln.query_sequence is not None
+            match = list(prebuilt_tag.iter(aln.query_sequence))
+            if match:
+                qnames.add(aln.query_name)
+    return qnames
+
+
+def _fish_multi_regions(
+    split_regions: list[tuple[str | int]],
+    bam_fspath,
+    prebuilt_tag,
+    nproc: int = 4,
+) -> set[str]:
+    logger.info("Fish sequence with tag pattern.")
+    start_t = time.time()
+    qnames = set()
+    with mp.Pool(processes=nproc) as pool:
+        for res in pool.imap_unordered(
+            partial(
+                _fish_one_region,
+                bam_fspath=bam_fspath,
+                prebuilt_tag=prebuilt_tag,
+            ),
+            split_regions,
+        ):
+            if res:
+                qnames = qnames | res
+    logger.info(f"Fish sequence with tag pattern: {time.time() - start_t} sec")
+    logger.info(f"Fished {len(qnames)} sequences with tag pattern.")
+    return qnames
+
+
+def _split_regions(
+    regions: dict[str, list[int]],
+    by: str,
+    n_splits: int = 4,
+    split_size: int = 500_000,
+) -> list[tuple[str | int]]:
+    if by not in ["len", "num"]:
+        raise ValueError(f"Unsupported split by method {by=}")
+    logger.info("Split regions into smaller intevals.")
+    splits = []
+    for sn, region in regions.items():
+        start, end = region
+        n_split = (
+            ((end - start + 1) // split_size) + 1 if by == "len" else n_splits
+        )
+        s = np.array_split(np.arange(end), n_split)
+        splits += [(sn, int(split[0]), int(split[-1])) for split in s]
+
+    logger.info(f"Split regions into {len(splits)} intevals.")
+    return splits
 
 
 def run_strawlr(
-    bam_fspath: _PathLike,
+    bam_fspath: str,
     tag_seq_fspath: _PathLike,
+    hla_bed_fspath: _PathLike,
     prebuild_method: str = "ahocorasick",
+    nproc: int = 4,
 ):
-    bam_fspath = parse_path(bam_fspath)
+    prebuilt_tag = build(tag_seq_fspath, method=prebuild_method)
 
-    prebuilt_tag = build(tag_seq_fspath)
-    # print(type(prebuilt_tag))
-    # print("TGCTGGAGTGTCCCAAGAGAGATGCAAAGTGTCTGAAT" in prebuilt_tag)
+    bametadata = BAMetadata(bam_fspath)
 
-    qnames_set_1 = _get_qnames_from_seq_with_tag(bam_fspath, prebuilt_tag)
-    print(len(qnames_set_1))
-    qnames_set_2 = _get_qnames_from_unplace_seq_with_tag(
-        bam_fspath, prebuilt_tag
+    fished_qnames = _fish_multi_hla(hla_bed_fspath, bam_fspath)
+
+    regions = {
+        sn: [1, ln] for sn, ln in bametadata.seqmap().items() if sn in CHR6
+    }
+    splits = _split_regions(regions, n_splits=nproc, by="num")
+    fished_qnames = fished_qnames | _fish_multi_regions(
+        splits, bam_fspath, prebuilt_tag, nproc
     )
-    print(len(qnames_set_2))
-    qnames_set_3 = _get_qnames_from_seq_mapped_to_class1(bam_fspath)
-    print(len(qnames_set_3))
+
+    fished_qnames = fished_qnames | _fish_unplaced(bam_fspath, prebuilt_tag)
+
+    logger.info("Fished {len(fished_qnames)} in total.")
