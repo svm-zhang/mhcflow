787d3923ab9d44a85fce1d3b5847004c02acba07
simo <svm.zhang@gmail.com>
2025-03-18 10:16:02 -0700

    Fixed error where duplicated sequences in fa_out; Added _novoindex().

--- src/mhcflow/finalizer.py
+++ src/mhcflow/finalizer.py
@@ -1,10 +1,32 @@
+import subprocess as sp
 from pathlib import Path
 
 import polars as pl
 from pyfaidx import Faidx
 from tinyscibio import make_dir, parse_path
 
-from .mhc_realigner import _run_realigner
+from .logger import logger
+from .realigner import _run_realigner
+
+
+def _novoindex(fa: Path) -> None:
+    nix = fa.with_suffix(".nix")
+    index_log = nix.with_suffix(".novoindex.log")
+    index_done = nix.with_suffix(".novoindex.done")
+    if index_done.exists():
+        logger.info(f"Found .nix index for Fasta file: {fa}. Skip.")
+        return
+    try:
+        cmd = ["novoindex", str(nix), str(fa)]
+        with open(index_log, "w") as f:
+            f.write(f"{' '.join(cmd)}\n")
+            p = sp.Popen(cmd, stdout=f, stderr=sp.STDOUT)
+            p.communicate()
+        index_done.touch()
+    except Exception as e:
+        print(e)
+        raise SystemExit
+    return
 
 
 def dump_seq(allele: str, fa: Faidx, out: Path):
@@ -36,8 +58,14 @@ def finalize(
     if fa_out.exists():
         fa_out.unlink()
     fa = Faidx(ref)
+    # do not forget to take unique for homozygous genotype of HLA gene.
+    # I dont want duplicated sequences in the fasta.
     _ = list(
-        map(lambda x: dump_seq(x, fa, fa_out), typer_res["allele"].to_list())
+        map(
+            lambda x: dump_seq(x, fa, fa_out),
+            typer_res["allele"].unique().to_list(),
+        )
     )
+    _novoindex(fa_out)
 
-    _run_realigner(fished_fqs, ref, outdir, rg, nproc)
+    _run_realigner(fished_fqs, fa_out, outdir, rg, nproc)
