9f8a020a39ceab12b7e70a3fca0944f65d10ad7b
simo <svm.zhang@gmail.com>
2025-03-21 09:01:48 -0700

    Updated run_finalizer signature.

--- src/mhcflow/finalizer.py
+++ src/mhcflow/finalizer.py
@@ -3,8 +3,9 @@ from pathlib import Path
 
 import polars as pl
 from pyfaidx import Faidx
-from tinyscibio import make_dir, parse_path
+from tinyscibio import BAMetadata, _PathLike, make_dir, parse_path
 
+from .helper import FileManifest, _check_rg_exists, _check_single_rg, _get_sm
 from .logger import logger
 from .realigner import _run_realigner
 
@@ -35,26 +36,37 @@ def dump_seq(allele: str, fa: Faidx, out: Path):
         f.write(f">{allele}\n{str(sequence)}\n")
 
 
-def finalize(
+def _run_finalizer(
+    bam_fspath: _PathLike,
+    ref: _PathLike,
+    fisher_fm: FileManifest,
+    realigner_fm: FileManifest,
     typer_res: pl.DataFrame,
-    ref: Path,
-    fished_fqs: list[tuple[Path, Path]],
-    rg: dict[str, str],
-    outdir: Path,
+    outdir: _PathLike,
     nproc: int = 1,
-):
+    overwrite: bool = False,
+) -> FileManifest:
+    bametadata = BAMetadata(str(bam_fspath))
+    _check_rg_exists(bametadata)
+    _check_single_rg(bametadata)
+    rg = bametadata.read_groups[0]
+    sm = _get_sm(rg)
+
+    finalizer_fm = FileManifest()
+
+    ref = parse_path(ref)
     fai = ref.parent / parse_path(f"{ref.name}.fai")
     if not fai.exists():
         raise FileNotFoundError
 
+    outdir = parse_path(outdir)
     make_dir(outdir, parents=True, exist_ok=True)
 
     # 3 locus * 2 alleles = 6
     if typer_res.shape[0] != 6:
         raise ValueError
 
-    sample = rg.get("SM")
-    fa_out = outdir / f"{sample}.hla.fasta"
+    fa_out = outdir / f"{sm}.hla.fasta"
     if fa_out.exists():
         fa_out.unlink()
     fa = Faidx(ref)
@@ -68,4 +80,5 @@ def finalize(
     )
     _novoindex(fa_out)
 
-    _run_realigner(fished_fqs, fa_out, outdir, rg, nproc)
+    # _run_realigner(fished_fqs, fa_out, outdir, rg, nproc)
+    return finalizer_fm
