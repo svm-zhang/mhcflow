d1bf0f263ac9a514926510e0495027e755dddab2
simo <svm.zhang@gmail.com>
2025-03-22 00:03:47 -0700

    Added clean and reset methods for FileManifest class; Added _verify_prev_run function.

--- src/mhcflow/helper.py
+++ src/mhcflow/helper.py
@@ -3,7 +3,7 @@ import json
 import sys
 from dataclasses import dataclass, field
 
-from tinyscibio import BAMetadata, _PathLike
+from tinyscibio import BAMetadata, _PathLike, parse_path
 
 from .logger import logger
 
@@ -87,9 +87,28 @@ class FileManifest:
         with open(json_out, "w") as f:
             json.dump(attrs, f)
 
-    # TODO: implement
-    def _clean(self) -> None:
-        raise NotImplementedError
+    def _reset(self) -> None:
+        for attr, attr_v in inspect.getmembers(FileManifest):
+            if not isinstance(attr_v, property):
+                continue
+            if attr == "inputs":
+                continue
+            self._clean_attr(attr)
+
+    def _clean_attrs(self, attrs: list[str]) -> None:
+        for attr in attrs:
+            self._clean_attr(attr)
+
+    def _clean_attr(self, attr: str) -> None:
+        to_be_cleaned = getattr(self, attr)
+        if not to_be_cleaned:
+            return
+        for k, v in to_be_cleaned.items():
+            if isinstance(v, list):
+                for f in v:
+                    parse_path(f).unlink(missing_ok=True)
+                continue
+            parse_path(v).unlink(missing_ok=True)
 
 
 def _check_rg_exists(bametadata: BAMetadata) -> None:
@@ -126,3 +145,38 @@ def _get_sm(rg: dict[str, str]) -> str:
     except ValueError as e:
         logger.error(e)
         sys.exit(1)
+
+
+def _verify_prev_run(fm: FileManifest, overwrite: bool = False) -> bool:
+    # if overwrite specify, destroy all files generated previously
+    # except everything in fm.inputs.
+    if overwrite:
+        logger.info("Overwrite specified. Remove results from previous run.")
+        fm._reset()
+        return False
+
+    logger.info("Verify manifest from previous run.")
+    done = parse_path(fm.aux.get("done", ""))
+    intermediate_dones = []
+    for k, v in fm.intermediate_aux.items():
+        if not k.endswith("done") and not k.endswith("dones"):
+            continue
+        intermediate_dones += v if isinstance(v, list) else [v]
+    not_exists = [
+        str(f) for f in intermediate_dones if not parse_path(f).exists()
+    ]
+    not_exists += [done] if not done.exists() else []
+    if not not_exists:
+        logger.info(
+            "Verified all done files for fisher from previous run. Skip."
+        )
+        return True
+    logger.info(
+        "Failed to verify previous run due to missing some done files: "
+        f"{not_exists}"
+    )
+    logger.info(
+        "Clean intermediate outputs and done files for a fresh new run."
+    )
+    fm._reset()
+    return False
